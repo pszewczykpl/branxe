b8c9dc23f38637a0bce8de009f638480098f0d73
Piotr Szewczyk <piotr.szewczyk96@gmail.com>
2019-08-24 21:59:04 +0200

    Usunięcie zbędnych plików z projektu

--- Bookmark Script.js
+++
@@ -1,6 +0,0 @@
-javascript:(function(){
-	branxe=document.createElement('script');
-	branxe.type='text/javascript';
-	branxe.src='http://localhost/branxe/script.php';
-	document.getElementsByTagName('head')[0].appendChild(branxe);
-})();
