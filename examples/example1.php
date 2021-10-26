7dad25ce3a55e1af45ccf79b94c84791e0ff68e7
pszewczykpl <pszewczykpl@gmail.com>
2021-10-26 03:26:32 +0200

    Added folder examples to store all use examples

---
+++ examples/example1.php
@@ -0,0 +1,17 @@
+<?php
+
+use Branxe\Script\Script;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$script = new Script;
+$script->insertMany(function ($action) {
+    $action->find_element_by_id('id_1')->click()->click();
+    $action->find_element_by_id('id_2')->click();
+    $action->find_element_by_id('id_3')->click();
+    $action->if('$("#id_4").length', function ($action) {
+        $action->find_element_by_id('id_5')->click();
+        $action->find_element_by_id('id_6')->click();
+    });
+});
+$script->render();
